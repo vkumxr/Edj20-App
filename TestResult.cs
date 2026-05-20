f3c386bb43df4976888a4aa2abf929207d14a8ee
Vishwa <vishwakumarv05@gmail.com>
2026-05-20 12:18:55 +0530

    Update TestResult.cs

--- TestResult.cs
+++ TestResult.cs
@@ -3,15 +3,10 @@ namespace Edj20Tester.Models
     public class TestResult
     {
         public string TestPoint { get; set; }
-
         public string TestType { get; set; }
-
         public double MeasuredValue { get; set; }
-
         public string Unit { get; set; }
-
         public string ExpectedRange { get; set; }
-
         public string Result { get; set; }
     }
 }
